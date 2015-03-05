09959509f1824683f4c9a53e595301b37a3b021d
haydar100 <haydar.yilmaz@student.hu.nl>
2015-03-05 14:52:06 +0100

    Nearest position gets colored blue, need to refactor code and put it into method

--- app/src/main/java/com/example/berkan/mapsprototype/ProtoTypeMaps.java
+++ app/src/main/java/com/example/berkan/mapsprototype/ProtoTypeMaps.java
@@ -9,15 +9,21 @@ import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ProtoTypeMaps extends FragmentActivity {
 
+
     public ArrayList<MarkerOptions> markers;
     public Handler handler;
+
+
+    public LatLng currentLocation;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
@@ -47,7 +53,9 @@ public class ProtoTypeMaps extends FragmentActivity {
      * <p/>
      * A user can return to this FragmentActivity after following the prompt and correctly
      * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
+     * have been
+     *
+     * completely destroyed during this process (it is likely that it would only be
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
@@ -63,6 +71,38 @@ public class ProtoTypeMaps extends FragmentActivity {
                 @Override
                 public void onMyLocationChange(Location location) {
                     LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                    double minDist = 1E38;
+                    int minIndex = -1;
+                    for (int i = 0; i < markers.size(); i++) {
+                        Location kaas = new Location("Marker");
+                        kaas.setLatitude(markers.get(i).getPosition().latitude);
+                        kaas.setLongitude(markers.get(i).getPosition().longitude);
+                        kaas.setTime(new Date().getTime());
+                        float test = location.distanceTo(kaas);
+                        if (test < minDist) {
+                            minDist = test;
+                            minIndex = i;
+                        }
+                    }
+                    if (minIndex >= 0) {
+                        MarkerOptions test2 = markers.get(minIndex);
+                        test2.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                        mMap.addMarker(test2);
+                        Log.d("Test2", "Test2 " + test2.getTitle());
+
+                    }
+
+/*
+                    for (int i = 0; i < markers.size(); i++) {
+                        float[] distance = new float[1];
+                        Location.distanceBetween(loc.latitude, loc.longitude, markers.get(i).getPosition().latitude, markers.get(i).getPosition().longitude, distance);
+                        //Log.d("Distance", "Distance of marker" + distance[0]);
+                        if(distance[0] < 100.0) {
+                            Log.d("Distance below 100 m", "Distance below 100 m" + distance[0]);
+                        }
+                    }*/
+
+
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 10.0f));
                     Log.d("Latitude", "Current Latitude " + location.getLatitude());
                     Log.d("Longitude", "Current Longitude " + location.getLongitude());
@@ -71,6 +111,7 @@ public class ProtoTypeMaps extends FragmentActivity {
             mMap.setOnMyLocationChangeListener(myLocationChangeListener);
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
+
                 setUpMap();
             }
         }
@@ -94,16 +135,39 @@ public class ProtoTypeMaps extends FragmentActivity {
             e.printStackTrace();
             Log.d("UI Thread and position thread merge failed", "Merging threads failed");
         }
-        addMarkersToMap(); // Causes Choreographer to skip frames, this can be fixed by seperating the addMarkerToMap methods to another class were ASyncTask is extended.
+
+        addMarkersToMap();
+        // Causes Choreographer to skip frames, this can be fixed by seperating the addMarkerToMap methods to another class were ASyncTask is extended.
 
     }
 
-    private void addMarkersToMap() {
+
+    public void addMarkersToMap() {
+
+
         Log.d("Size of markers after join ", "Size of markers inside UI Thread" + markers.size());
         for (MarkerOptions m : markers) {
             mMap.addMarker(m);
         }
     }
 
+    public void CalculateRadius() {
 
+    }
+
+    public ArrayList<MarkerOptions> getMarkers() {
+        return markers;
+    }
+
+    public void setMarkers(ArrayList<MarkerOptions> markers) {
+        this.markers = markers;
+    }
+
+    public LatLng getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(LatLng currentLocation) {
+        this.currentLocation = currentLocation;
+    }
 }
